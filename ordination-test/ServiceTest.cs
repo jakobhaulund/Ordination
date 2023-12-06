8e5d1ffeccb505d4db6284977daa68994afc182b
jakobhaulund <jakobhaulund@hotmail.com>
2023-12-06 09:33:30 +0100

    teztz

--- ordination-test/ServiceTest.cs
+++ ordination-test/ServiceTest.cs
@@ -42,10 +42,49 @@ public class ServiceTest
     }
 
     [TestMethod]
+    public void OpretPN()
+    {
+        Patient patient = service.GetPatienter().First();
+        Laegemiddel lm = service.GetLaegemidler().First();
+
+        Assert.AreEqual(4, service.GetPNs().Count());
+
+        service.OpretPN(patient.PatientId, lm.LaegemiddelId, 14, DateTime.Now, DateTime.Now.AddDays(3));
+
+        Assert.AreEqual(5, service.GetPNs().Count());
+    }
+
+    [TestMethod]
+    public void OpretDagligSkæv()
+    {
+        Patient patient = service.GetPatienter().First();
+        Laegemiddel lm = service.GetLaegemidler().First();
+
+        Assert.AreEqual(1, service.GetDagligSkæve().Count());
+
+        Dosis[] dosisArray = new Dosis[]
+        {
+        new Dosis(DateTime.Now, 2),
+        new Dosis(DateTime.Now.AddHours(2), 2),
+        new Dosis(DateTime.Now.AddHours(4), 2),
+        new Dosis(DateTime.Now.AddHours(6), 2)
+        };
+
+        service.OpretDagligSkaev(patient.PatientId, lm.LaegemiddelId, dosisArray, DateTime.Now, DateTime.Now.AddDays(3));
+
+        Assert.AreEqual(2, service.GetDagligSkæve().Count());
+    }
+
+
+
+
+    [TestMethod]
     [ExpectedException(typeof(ArgumentNullException))]
     public void TestAtKodenSmiderEnException()
     {
         service.GetAnbefaletDosisPerDøgn(-1, 20);
     }
+
+
 
 }
